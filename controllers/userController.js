481bd921d354fc0082e07ff6d9bd1f3d8016c028
habyarimana caleb <callebhabyar55@gmail.com>
2025-03-13 01:13:14 +0200

    handle way user json is sended to dashboard

--- controllers/userController.js
+++ controllers/userController.js
@@ -200,7 +200,7 @@ exports.login = async (req, res) => {
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find({});
-    res.status(200).json({ "Our clients are": users });
+    res.status(200).json(users);
   } catch (error) {
     console.error("Error fetching users:", error);
     res.status(500).json({ message: "Server error", error: error.message });
