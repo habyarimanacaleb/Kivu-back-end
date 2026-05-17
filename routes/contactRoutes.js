d6549d0873179f081eeed9d6aa7ecfa38101b129
habyarimanacaleb <callebhabyar55@gmail.com>
2026-05-17 03:23:55 +0200

    Fixing contact endpoint issue

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -7,8 +7,8 @@ const verifyToken = require("../middleware/verifyUserRole");
 
 router.post("/contact", contactController.createContact);
 router.get("/contacts", contactController.getAllContacts);
-router.post("contact/respond",verifyToken(["admin"]), contactController.respondToContact);
-router.delete("contact/:id", verifyToken(["admin"]), contactController.deleteContact);
-router.delete("contacts/", verifyToken(["admin"]), contactController.deleteAllContacts); 
+router.post("/contact/respond",verifyToken(["admin"]), contactController.respondToContact);
+router.delete("/contact/:id", verifyToken(["admin"]), contactController.deleteContact);
+router.delete("/contacts/", verifyToken(["admin"]), contactController.deleteAllContacts); 
 
 module.exports = router;
