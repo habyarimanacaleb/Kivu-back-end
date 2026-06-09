d79e241a023bfffdd07a440a48001150e437b056
habyarimanacaleb <callebhabyar55@gmail.com>
2026-06-09 22:38:53 +0200

    Now we have production grade app by adding image resizing middleware to optimize images before uploading to cloudinary. This will help reduce the load on the server and improve the performance of the app. We have also added compression middleware to compress the response data before sending it to the client, which will further improve the performance of the app. Additionally, we have implemented socket.io for real-time communication between the server and clients, which will allow us to implement features like live chat or notifications in the future. Overall, these changes will help make our app more efficient and responsive for our users.

--- routes/ServiceRoutes.js
+++ routes/ServiceRoutes.js
@@ -3,6 +3,7 @@ const serviceController = require("../controllers/serviceController");
 const upload = require("../middleware/upload");
 const verifyAdmin = require("../middleware/adminAuth");
 const router = express.Router();
+const resizeTourImage = require("../middleware/resizeImage");
 
 const serviceUploadFields = upload.fields([
   { name: "imageFile", maxCount: 1 },
@@ -15,8 +16,8 @@ router.get('/images', serviceController.getServiceImages);
 router.get("/:id", serviceController.getServiceById);
 
 // Secured Administrative Control Gates
-router.post("/", verifyAdmin, serviceUploadFields, serviceController.createService);
-router.put("/:id", verifyAdmin, serviceUploadFields, serviceController.updateServiceById);
+router.post("/", verifyAdmin, serviceUploadFields,resizeTourImage, serviceController.createService);
+router.put("/:id", verifyAdmin, serviceUploadFields, resizeTourImage, serviceController.updateServiceById);
 router.delete("/:id", verifyAdmin, serviceController.deleteServiceById);
 
 module.exports = router;
